4c79d48db236b5e479c5777d0fb9bce0411f33b9
ilikeamoney <2721000@naver.com>
2023-05-25 16:18:59 +0900

    modify front

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+  import {ref} from "vue";
+
+  import axios from "axios";
+  import {useRouter} from "vue-router";
+
+  const title = ref("")
+  const content = ref("")
+
+  const router = useRouter()
+
+  const write = function () {
+    axios.post("/api/posts", {
+      title : title.value,
+      content : content.value
+    }).then(() => {
+          router.replace({name: "home"})
+        });
+  };
+
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해 주세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input type="textarea" v-model="content" rows="15"/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글작성 완료</el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
--- src/main/java/hello/Spring/api/controller/ExceptionController.java
+++ src/main/java/hello/Spring/api/controller/ExceptionController.java
@@ -64,5 +64,4 @@ public class ExceptionController {
 
         return response;
     }
-
 }
